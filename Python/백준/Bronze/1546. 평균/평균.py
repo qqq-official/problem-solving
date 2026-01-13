dd4f3937be5bfd355de543484711f9b7f6a2c317
QQQ <qqqofficial1125@gmail.com>
2026-01-13 20:35:24 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1546. 평균/평균.py
+++ Python/백준/Bronze/1546. 평균/평균.py
@@ -1,4 +1,3 @@
-input()
-
-ns = list(map(int, input().split()))
-print(sum(map(lambda x: x / max(ns) * 100, ns)) / len(ns))
+n = int(input())
+s = list(map(int, input().split()))
+print(sum(s) * 100 / max(s) / n)
